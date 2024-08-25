2714693cb54531546111f1cd3a0159f085bc83e2
GDHui <WU13712220836@163.com>
2024-08-25 17:07:05 +0800

    堆

---
+++ Heap.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+#ifndef MAXHEAP
+#define MAXHEAP
+
+class MaxHeap
+{
+private:
+    std::vector<int> maxHeap;
+
+public:
+    bool IsEmpty();
+
+    int Left(int i);
+    int Right(int i);
+    int Parent(int i);
+    int Peek();
+
+    void Push(int val);
+    void SiftUp(int i);
+
+    void Pop();
+    void SiftDown(int i);
+}
+
+#endif MAXHEAP
