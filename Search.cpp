680080725e637b12bd7d7c25a2ec1e77ea051a17
GDHui <WU13712220836@163.com>
2024-08-29 10:07:41 +0800

    二分查找插入点与边界

--- Search.cpp
+++ Search.cpp
@@ -5,7 +5,7 @@ int BinarySearch(std::vector<int> &nums, int target)
     int i = 0, j = nums.size() - 1;
     while (i < j)
     {
-        int m = (i + (j - i)) / 2;
+        int m = i + (j - i) / 2;
         if (nums[m] < target)
         {
             i = m + 1;
@@ -22,12 +22,12 @@ int BinarySearch(std::vector<int> &nums, int target)
     return -1;
 }
 
-int BInarySearchLCRO(std::vector<int> &nums, int target)
+int BinarySearchLCRO(std::vector<int> &nums, int target)
 {
     int i = 0, j = nums.size() - 1;
     while (i < j)
     {
-        int m = (i + (j - i)) / 2;
+        int m = i + (j - i) / 2;
         if (nums[m] < target)
         {
             i = m + 1;
@@ -42,4 +42,69 @@ int BInarySearchLCRO(std::vector<int> &nums, int target)
         }
     }
     return -1;
+}
+
+int BinarySearchInsertionSimple(std::vector<int> &nums, int target)
+{
+    int i = 0, j = nums.size() - 1;
+    while (i < = j)
+    {
+        int m = i + (j - i) / 2;
+        if (nums[m] < target)
+        {
+            i = m + 1;
+        }
+        else if (nums[m] > target)
+        {
+            j = m - 1;
+        }
+        else
+        {
+            return m;
+        }
+    }
+    return i;
+}
+
+int BinarySearchInsertion(std::vector<int> &nums, int target)
+{
+    int i = 0, j = nums.size() - 1;
+    while (i <= j)
+    {
+        int m = i + (j - 1) / 2;
+        if (nums[m] < target)
+        {
+            i = m + 1;
+        }
+        else if (nums[m] > target)
+        {
+            j = m - 1;
+        }
+        else
+        {
+            j = m - 1;
+        }
+    }
+    return i;
+}
+
+int BinarySearchLeftEdge(std::vector<int> &nums, int target)
+{
+    int i = BinarySearchInsertion(nums, target);
+    if (i == nums.size() || nums[i] != target)
+    {
+        return -1;
+    }
+    return i;
+}
+
+int BinarySearchRightEdge(std::vector<int> &nums, int target)
+{
+    int i = BinarySearchInsertion(nums, target + 1);
+    int j = i - 1;
+    if (j == -1 || nums[j] != target)
+    {
+        return -1;
+    }
+    return j;
 }
--- Search.h
+++ Search.h
@@ -2,4 +2,8 @@
 #include <vector>
 
 int BinarySearch (std::vector<int> & nums,int target);
-int BInarySearchLCRO(std::vector<int> &nums, int target);
+int BinarySearchLCRO(std::vector<int> &nums, int target);
+int BinarySearchInsertionSimple(std::vector<int> &nums, int target);
+int BinarySearchInsertion(std::vector<int> &nums, int target);
+int BinarySearchLeftEdge(std::vector<int> &nums, int target);
+int BinarySearchRightEdge(std::vector<int> &nums, int target);
