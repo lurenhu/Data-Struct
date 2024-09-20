709d5691e07156944604626f7a95c2bad50afbba
GDHui <WU13712220836@163.com>
2024-09-20 18:15:58 +0800

    编辑距离问题

--- DynamicProgramming.cpp
+++ DynamicProgramming.cpp
@@ -109,3 +109,24 @@ int CoinChangeIIDP(std::vector<int> &coins, int amt) {
     }
     return dp[n][amt];
 }
+
+int EditDistanceDP(std::string s, std::string t) {
+    int n = s.length(), m = t.length();
+    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
+    for (int i = 1; i <= n; i++) {
+        dp[i][0] = i;
+    }
+    for (int j = 1; j <= m; j++) {
+        dp[0][j] = j;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s[i - 1] == t[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = std::min(dp[i - 1][j - 1] + 1, dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
--- DynamicProgramming.h
+++ DynamicProgramming.h
@@ -8,3 +8,4 @@ int KnapsackDPComp(std::vector<int> &wgt, std::vector<int> &val, int cap);
 int UnboundedKnapsackDP(std::vector<int> &wgt, std::vector<int> &val, int cap);
 int CoinChangeDP(std::vector<int> &coins, int amt);
 int CoinChangeIIDP(std::vector<int> &coins, int amt);
+int EditDistanceDP(std::string s, std::string t);
